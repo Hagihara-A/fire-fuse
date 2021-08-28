b354830ce44746c142d37d674debfad9f50a3648
Arark <knkr5456@gmail.com>
2021-08-28 23:28:10 +0900

    feat: impl updateDoc

--- firefuse/src/index.ts
+++ firefuse/src/index.ts
@@ -1,13 +1,26 @@
 import * as firestore from "firebase/firestore";
+import { DocumentReference, Timestamp } from "firebase/firestore";
 
+type FieldType =
+  | string
+  | number
+  | boolean
+  | null
+  | Timestamp
+  | FieldType[]
+  | DocumentData;
+
+type DocumentData = {
+  [K: string]: FieldType;
+};
 export type SchemaBase = {
   [K in string]: {
-    doc: firestore.DocumentData;
+    doc: DocumentData;
     subcollection?: SchemaBase;
   };
 };
 export type Collection<
-  T extends firestore.DocumentData,
+  T extends DocumentData,
   SC extends SchemaBase | undefined = undefined
 > = {
   doc: T;
@@ -15,38 +28,27 @@ export type Collection<
 };
 
 export type CollectionPaths<S extends SchemaBase> = {
-  [K in string & keyof S]: S[K]["subcollection"] extends SchemaBase
+  [K in keyof S]: S[K]["subcollection"] extends SchemaBase
     ? [K] | [K, string, ...CollectionPaths<S[K]["subcollection"]>]
     : [K];
-}[string & keyof S];
+}[keyof S];
 
 export type DocumentPaths<S extends SchemaBase> = [
   ...CollectionPaths<S>,
   string
 ];
 
-export type GetCollectionData<
+export type GetData<
   S extends SchemaBase,
-  P extends string[] & CollectionPaths<S>
-> = P extends [infer C]
+  P extends CollectionPaths<S> | DocumentPaths<S>
+> = P extends [infer C] | [infer C, string]
   ? S[C & string]["doc"]
   : P extends [infer C, string, ...infer Rest]
   ? S[C & string]["subcollection"] extends SchemaBase
-    ? Rest extends string[] & CollectionPaths<S[C & string]["subcollection"]>
-      ? GetCollectionData<S[C & string]["subcollection"], Rest>
-      : never
-    : never
-  : never;
-
-export type GetDocumentData<
-  S extends SchemaBase,
-  P extends DocumentPaths<S>
-> = P extends [infer C, string]
-  ? S[C & string]["doc"]
-  : P extends [infer C, string, ...infer Rest]
-  ? S[C & string]["subcollection"] extends SchemaBase
-    ? Rest extends DocumentPaths<S[C & string]["subcollection"]>
-      ? GetDocumentData<S[C & string]["subcollection"], Rest>
+    ? Rest extends
+        | CollectionPaths<S[C & string]["subcollection"]>
+        | DocumentPaths<S[C & string]["subcollection"]>
+      ? GetData<S[C & string]["subcollection"], Rest>
       : never
     : never
   : never;
@@ -58,5 +60,46 @@ export const collection =
     ...paths: P
   ) =>
     firestore.collection(DB, paths.join("/")) as firestore.CollectionReference<
-      GetCollectionData<S, P>
+      GetData<S, P>
     >;
+export const doc =
+  <S extends SchemaBase>() =>
+  <P extends DocumentPaths<S>>(DB: firestore.Firestore, ...paths: P) =>
+    firestore.doc(DB, paths.join("/")) as firestore.DocumentReference<
+      GetData<S, P>
+    >;
+
+export type DeepKeys<T extends DocumentData> = {
+  [K in keyof T]: T[K] extends DocumentData
+    ? [K, ...DeepKeys<T[K]>] | [K]
+    : [K];
+}[keyof T];
+
+export type Join<T extends string[]> = T extends [infer H]
+  ? `${H & string}`
+  : T extends [infer H, ...infer Rest]
+  ? Rest extends string[]
+    ? `${H & string}.${Join<Rest> & string}`
+    : never
+  : never;
+export type UpdateKeys<T extends DocumentData> = Join<
+  DeepKeys<T> extends string[] ? DeepKeys<T> : never
+>;
+export type UpdateData<T extends DocumentData> = {
+  [K in UpdateKeys<T>]?: K extends `${infer H}.${string}`
+    ? T[H] extends DocumentData
+      ? UpdateData<T[H]>
+      : never
+    : K extends keyof T
+    ? T[K]
+    : never;
+};
+export type DeepPartial<T extends DocumentData> = {
+  [K in keyof T]?: T[K] extends DocumentData ? DeepPartial<T[K]> : T[K];
+};
+export const updateDoc = <
+  T extends DocumentData
+>(
+  doc: DocumentReference<T>,
+  data: UpdateData<T> | DeepPartial<T>
+) => firestore.updateDoc(doc, data as any);
