b465b7928672e5ad6c6efcc4e44c97d61ae770b6
Arark <knkr5456@gmail.com>
2021-08-28 23:46:15 +0900

    test(doc, updateDoc): update partially, overwrite entirely

--- firefuse/src/index.test.ts
+++ firefuse/src/index.test.ts
@@ -5,6 +5,7 @@ import {
   addDoc,
   getDoc,
   terminate,
+  setDoc,
 } from "firebase/firestore";
 import * as fuse from "./index";
 import { Collection } from ".";
@@ -23,7 +24,16 @@ type Payment = {
 };
 type Room = {
   size: number;
+  rooms: {
+    living: number;
+    dining: number;
+    kitchen: number;
+  };
+  level1: {
+    level2_1: string[];
+  };
 };
+
 type C1 = {
   c1: "c1";
 };
@@ -34,6 +44,7 @@ type C2 = {
 
 type C3 = {
   c3: "c3";
+  c31: { c32: { c33: string; c33_2: number } };
 };
 
 type MySchema = {
@@ -41,11 +52,17 @@ type MySchema = {
   room: Collection<Room>;
   C1: Collection<C1, { C2: Collection<C2, { C3: Collection<C3> }> }>;
 };
+
+const fcollection = fuse.collection<MySchema>();
+const fdoc = fuse.doc<MySchema>();
 afterAll(async () => {
   await terminate(DB);
 });
-test("collection.add", async () => {
+
+test("confirm is emulator running", async () => {
   const colRef = collection(DB, "a/b/c");
+  expect(colRef.id).toBe("c");
+  expect(colRef.parent.parent.id).toBe("a");
   const data = { test: "test" };
   const doc = await addDoc(colRef, data);
 
@@ -54,14 +71,84 @@ test("collection.add", async () => {
 });
 
 describe("collection", () => {
-  test("add shallow collection & read", async () => {
-    const fcollection = fuse.collection<MySchema>();
-
+  test("add nested collection & read", async () => {
     const paymentRef = fcollection(DB, "user", "a", "payment");
+    expect(paymentRef.path).toBe("user/a/payment");
     const payment: Payment = { cardNumber: 1234 };
     const docRef = await addDoc(paymentRef, payment);
 
     const savedDoc = await getDoc(docRef);
     expect(savedDoc.data()).toEqual(payment);
+  });
+});
+
+describe("doc", () => {
+  test("create nested document & read it", async () => {
+    const docRef = fdoc(DB, "user", "a", "payment", "b");
+    expect(docRef.id).toBe("b");
+
+    const data = { cardNumber: 1234 };
+    await setDoc(docRef, data);
+
+    const savedDoc = await getDoc(docRef);
+    expect(savedDoc.data()).toEqual(data);
+  });
+
+  test("update nested doc partially", async () => {
+    const docRef = fdoc(DB, "C1", "c1", "C2", "c2", "C3", "c3");
+    expect(docRef.id).toBe("c3");
+
+    const data = {
+      c3: "c3",
+      c31: { c32: { c33: "c33", c33_2: 123 } },
+    };
+    await setDoc(docRef, data);
+    await fuse.updateDoc(docRef, { "c31.c32.c33": "update" });
+    const updatedData = (await getDoc(docRef)).data();
+    const expectedData: C3 = {
+      c3: "c3",
+      c31: { c32: { c33: "update", c33_2: 123 } },
+    };
+    expect(updatedData).toEqual(expectedData);
+  });
+
+  test("overwrite nested value", async () => {
+    const docRef = fdoc(DB, "C1", "c1", "C2", "c2", "C3", "c3");
+    expect(docRef.id).toBe("c3");
+
+    const data = {
+      c3: "c3",
+      c31: { c32: { c33: "c33", c33_2: 123 } },
+    };
+    await setDoc(docRef, data);
+    await fuse.updateDoc(docRef, {
+      c31: { c32: { c33: "update", c33_2: 321 } },
+    });
+    const updatedData = (await getDoc(docRef)).data();
+    const expectedData: C3 = {
+      c3: "c3",
+      c31: { c32: { c33: "update", c33_2: 321 } },
+    };
+    expect(updatedData).toEqual(expectedData);
+  });
+
+  test("disapper unspecified value of nested object", async () => {
+    const docRef = fdoc(DB, "C1", "c1", "C2", "c2", "C3", "c3");
+    expect(docRef.id).toBe("c3");
+
+    const data = {
+      c3: "c3",
+      c31: { c32: { c33: "c33", c33_2: 123 } },
+    };
+    await setDoc(docRef, data);
+    await fuse.updateDoc(docRef, {
+      c31: { c32: { c33: "update" } },
+    });
+    const updatedData = (await getDoc(docRef)).data();
+    const expectedData: C3 = {
+      c3: "c3",
+      c31: { c32: { c33: "update", c33_2: undefined } },
+    };
+    expect(updatedData).toEqual(expectedData);
   });
 });
