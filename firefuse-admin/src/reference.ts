940f894765e8bd240b3856a1525fff64439dd809
arark <knkr5456@gmail.com>
2021-10-18 00:56:02 +0900

    feat(reference): impl UpdateData using Key Remapping

--- firefuse-admin/src/reference.ts
+++ firefuse-admin/src/reference.ts
@@ -1,40 +1,52 @@
-import admin from "firebase-admin";
+import * as firestore from "firebase-admin/firestore";
 import { DocumentData, StrKeyof } from ".";
+
 export interface FuseDocumentReference<T extends DocumentData>
-  extends admin.firestore.DocumentReference<T> {
+  extends firestore.DocumentReference<T> {
   update(
     data: UpdateData<T>,
-    precondition?: admin.firestore.Precondition
-  ): Promise<admin.firestore.WriteResult>;
+    precondition?: firestore.Precondition
+  ): Promise<firestore.WriteResult>;
 
   update(
-    field: string | admin.firestore.FieldPath,
+    field: string | firestore.FieldPath,
     value: unknown,
     ...moreFieldsOrPrecondition: unknown[]
-  ): Promise<admin.firestore.WriteResult>;
+  ): Promise<firestore.WriteResult>;
 }
 
-export type UpdateData<T extends DocumentData, UP extends string= UpdatePaths<T>> = {
-  [P in UP]?: UpdateValue<T, P>;
+export type UpdateData<T extends DocumentData> = {
+  [P in UpdatePaths<T> as Join<P>]: UpdateValue<T, P>;
 };
 
+export type Join<P extends string[]> = P extends [infer Head, ...infer Rest]
+  ? Head extends string
+    ? Rest extends []
+      ? Head
+      : Rest extends [string, ...string[]]
+      ? `${Head}.${Join<Rest>}`
+      : never
+    : never
+  : never;
 export type UpdatePaths<T extends DocumentData> = {
   [K in StrKeyof<T>]: T[K] extends DocumentData
-    ? `${K}` | `${K}.${UpdatePaths<T[K]>}`
-    : `${K}`;
+    ? [K] | [K, ...UpdatePaths<T[K]>]
+    : [K];
 }[StrKeyof<T>];
 
 export type UpdateValue<
   T extends DocumentData,
   P extends UpdatePaths<T>
-> = P extends `${infer Head}.${infer Rest}`
-  ? T[Head] extends DocumentData
-    ? Rest extends UpdatePaths<T[Head]>
-      ? UpdateValue<T[Head], Rest>
+> = P extends [infer Head, ...infer Rest]
+  ? Head extends keyof T
+    ? T[Head] extends infer U
+      ? Rest extends []
+        ? U
+        : U extends DocumentData
+        ? Rest extends UpdatePaths<U>
+          ? UpdateValue<U, Rest>
+          : never
+        : never
       : never
     : never
-  : P extends keyof T
-  ? T[P]
   : never;
-type P = UpdatePaths<{ a: { b: { c: number } }; d: string }>;
-type V = UpdateValue<{ a: { b: { c: number } }; d: string }, "a">;
