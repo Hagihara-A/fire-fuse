be92b5f10257861ad7345f9c23e118188480e868
Arark <knkr5456@gmail.com>
2021-11-05 03:28:05 +0900

    test(update): add tests of UpdatePaths and UpdateData

--- firefuse-admin/src/update.test.ts
+++ firefuse-admin/src/update.test.ts
@@ -1,18 +1,30 @@
-import { DocumentReference } from "firebase-admin/firestore";
 import { Assert, DB, Extends, Match, Room } from "./index.test";
-import { UpdateData } from "./update.js";
+import { UpdateData, UpdatePaths } from "./update.js";
 
+describe(`UpdatePaths`, () => {
+  test(`"size" extends UpdatePaths<Room>`, () => {
+    type P = UpdatePaths<Room>;
+    type _ = Assert<Extends<P, "size">>;
+  });
+  test(`"rooms.living" extends UpdatePaths<Room>`, () => {
+    type P = UpdatePaths<Room>;
+    type _ = Assert<Extends<P, "rooms.living">>;
+  });
+});
 describe("update", () => {
   describe("UpdateData", () => {
     test(`UpdateData<City> contains { "rooms.living": number }`, () => {
       type A = UpdateData<Room>;
-      type _ = Assert<Match<{ "rooms.living": number }, A>>;
+      type _ = Assert<Match<{ "rooms.living"?: number }, A>>;
     });
 
     test(`UpdateData<City> contains { rooms : {...} }`, () => {
       type A = UpdateData<Room>;
       type _ = Assert<
-        Match<{ rooms: { living?: number; dining?: number; kitchen?: number } }, A>
+        Match<
+          { rooms?: { living?: number; dining?: number; kitchen?: number } },
+          A
+        >
       >;
     });
   });
