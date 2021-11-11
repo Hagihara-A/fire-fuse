e4bc6b8188228244628e5cb7a15ec2e423a0bc53
Arark <knkr5456@gmail.com>
2021-11-11 16:39:28 +0900

    test(collection): use Collection interface

--- firefuse/src/collection.test.ts
+++ firefuse/src/collection.test.ts
@@ -1,8 +1,8 @@
 import * as fs from "firebase/firestore";
-import { collection as C } from "./collection.js";
+import { Collection } from "./collection.js";
 import { Assert, City, DB, Exact, MySchema } from "./index.test.js";
 
-export const collection = C<MySchema>();
+export const collection = fs.collection as Collection<MySchema>;
 
 test(`collection(DB, "cities") returuns CollectionReference<City>`, () => {
   const cities = collection(DB, "cities");
