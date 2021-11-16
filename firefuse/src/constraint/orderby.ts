568b8a43b98b870443e019d3588a7d593ab967a2
Arark <knkr5456@gmail.com>
2021-11-16 23:12:11 +0900

    refactor(KeyofOrderble): move to orederby.ts

--- firefuse/src/constraint/orderby.ts
+++ firefuse/src/constraint/orderby.ts
@@ -1,16 +1,22 @@
 import * as firestore from "firebase/firestore";
 
-import { DocumentData, KeyofPrimitive } from "../index.js";
+import { DocumentData } from "../index.js";
+import { Orderable } from "./where.js";
 
+export type KeyofOrderable<T extends DocumentData> = keyof T extends infer K
+  ? K extends Orderable
+    ? K
+    : never
+  : never;
 export interface OrderBy<T extends DocumentData> {
-  <F extends KeyofPrimitive<T> & string>(
+  <F extends KeyofOrderable<T> & string>(
     field: F,
     order?: firestore.OrderByDirection
   ): OrderByConstraint<F>;
 }
 
 export const orderBy = <T extends DocumentData>(): OrderBy<T> => {
-  return <F extends KeyofPrimitive<T> & string>(
+  return <F extends KeyofOrderable<T>>(
     field: F,
     order?: firestore.OrderByDirection
   ) => firestore.orderBy(field, order) as OrderByConstraint<F>;
--- firefuse/src/index.ts
+++ firefuse/src/index.ts
@@ -49,12 +49,6 @@ export type GetData<
     : never
   : never;
 
-export type KeyofPrimitive<
-  T extends DocumentData,
-  K extends keyof T = keyof T
-> = {
-  [L in K]: T[L] extends FieldType[] | DocumentData ? never : L;
-}[K];
 
 export type ExcUndef<T> = Exclude<T, undefined>;
 
