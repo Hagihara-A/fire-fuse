4c26654c7f964e903a32a8d23d4031dab91afcda
Arark <knkr5456@gmail.com>
2022-03-06 15:19:10 +0900

    test(GetData): tests for `GetData`

---
+++ firefuse/src/GetData.test.ts
@@ -0,0 +1,28 @@
+import { GetData } from "./GetData.js";
+import { Assert, Exact, MySchema } from "./index.test.js";
+
+describe(`GetData`, () => {
+  test(`GetData<MySchema, ["user"]> is exactlly User | Count`, () => {
+    type D = GetData<MySchema, ["user"]>;
+    type _ = Assert<Exact<D, MySchema["user"][string]["doc"]>>;
+  });
+
+  test(`GetData<MySchema, ["user", string]> is exactlly User | Count`, () => {
+    type D = GetData<MySchema, ["user", string]>;
+    type _ = Assert<Exact<D, MySchema["user"][string]["doc"]>>;
+  });
+
+  test(`GetData<MySchema, ["cities", "v1", "cities"]> is exactlly City`, () => {
+    type D = GetData<MySchema, ["cities", "v1", "cities"]>;
+    type _ = Assert<
+      Exact<D, MySchema["cities"]["v1"]["col"]["cities"][string]["doc"]>
+    >;
+  });
+
+  test(`GetData<MySchema, ["cities", "v1", "cities", string]> is exactlly City`, () => {
+    type D = GetData<MySchema, ["cities", "v1", "cities", string]>;
+    type _ = Assert<
+      Exact<D, MySchema["cities"]["v1"]["col"]["cities"][string]["doc"]>
+    >;
+  });
+});
