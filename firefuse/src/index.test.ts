4b31159cc356d0b346fa333517f4f238f4b04b39
Arark <knkr5456@gmail.com>
2022-03-06 15:49:51 +0900

    test(index): change `MySchema` to cover more cases

--- firefuse/src/index.test.ts
+++ firefuse/src/index.test.ts
@@ -1,20 +1,52 @@
 import * as fs from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { Defined } from "./index.js";
+import { Defined, Schema } from "./index.js";
 import { Doc } from "./doc.js";
 import { Collection } from "./collection.js";
+import * as fst from "firebase/firestore";
 
 const app = initializeApp({ projectId: "pid" });
 export const DB = fs.getFirestore(app);
 fs.connectFirestoreEmulator(DB, "localhost", 8080);
 
+export type MySchema = {
+  user: {
+    [DocKey: string]: {
+      doc: User | Count;
+      col?: {
+        favRooms: { [DocKey: string]: { doc: Room } };
+      };
+    };
+    count: {
+      doc: Count;
+    };
+  };
+  cities: {
+    v1: {
+      doc: Record<string, never>;
+      col: {
+        cities: {
+          [DocKey: string]: { doc: City };
+        };
+      };
+    };
+    v2: {
+      doc: Record<string, never>;
+      col: {
+        cities: {
+          [DocKey: string]: { doc: CityV2 };
+        };
+      };
+    };
+  };
+};
+
 export type User = {
   name: string;
   age: number;
+  sex: "male" | "female" | "other";
 };
-export type Payment = {
-  cardNumber: number;
-};
+
 export type Room = {
   size: number;
   rooms: {
@@ -24,26 +56,9 @@ export type Room = {
   };
   city: fs.DocumentReference<City>;
 };
-export type TsTestData = {
-  ts?: fs.Timestamp;
-};
 
-export type MySchema = {
-  user: {
-    doc: User;
-    subcollection: {
-      payment: { doc: Payment };
-    };
-  };
-  room: {
-    doc: Room;
-  };
-  cities: {
-    doc: City;
-  };
-  ts: {
-    doc: TsTestData;
-  };
+export type Count = {
+  allDocumentCount?: number;
 };
 
 export type City = {
@@ -53,6 +68,20 @@ export type City = {
   capital?: boolean;
   population?: number;
   regions?: string[];
+};
+
+export type CityV2 = {
+  name: string;
+  state: string | null;
+  country: string;
+  capital?: boolean;
+  population?: number;
+  regions?: string[];
+  history: {
+    createdAt: fst.Timestamp;
+    updatedAt?: fst.Timestamp[];
+  };
+  cityV1Ref?: fs.DocumentReference<City>;
 };
 
 export type Extends<A, E> = [A] extends [E] ? true : false;
@@ -67,63 +96,18 @@ export type Never<T> = T extends never ? true : false;
 
 export type Assert<T extends true> = T;
 
-export const doc = fs.doc as Doc<MySchema>;
-export const collection = fs.collection as Collection<MySchema>;
+export const doc = fs.doc as unknown as Doc<MySchema>;
+export const collection = fs.collection as unknown as Collection<MySchema>;
 
 afterAll(async () => {
   await fs.terminate(DB);
 });
 
-beforeAll(async () => {
-  const addDataEntries: Record<string, City> = {
-    SF: {
-      name: "San Francisco",
-      state: "CA",
-      country: "USA",
-      capital: false,
-      population: 860000,
-      regions: ["west_coast", "norcal"],
-    },
-    LA: {
-      name: "Los Angeles",
-      state: "CA",
-      country: "USA",
-      capital: false,
-      population: 3900000,
-      regions: ["west_coast", "socal"],
-    },
-    DC: {
-      name: "Washington, D.C.",
-      state: null,
-      country: "USA",
-      capital: true,
-      population: 680000,
-      regions: ["east_coast"],
-    },
-    TOK: {
-      name: "Tokyo",
-      state: null,
-      country: "Japan",
-      capital: true,
-      population: 9000000,
-      regions: ["kanto", "honshu"],
-    },
-    BJ: {
-      name: "Beijing",
-      state: null,
-      country: "China",
-      capital: true,
-      population: 21500000,
-      regions: ["jingjinji", "hebei"],
-    },
-  };
-  const batch = fs.writeBatch(DB);
-  for (const [k, ent] of Object.entries(addDataEntries)) {
-    batch.set(fs.doc(fs.collection(DB, "cities")), ent);
-  }
-  await batch.commit();
-});
 test(`Defined<{a?: string}, "a"> is {a: string}`, () => {
   type T = Defined<{ a?: string }, "a">;
   type _ = Assert<Exact<{ a: string }, T>>;
+});
+
+test(`MySchema extends Schema`, () => {
+  type _ = Assert<Extends<MySchema, Schema>>;
 });
