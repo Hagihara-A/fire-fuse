1c52dad4964917549fa8ecd034acbae45b35d02a
Arark <knkr5456@gmail.com>
2022-03-06 15:25:55 +0900

    test(collection): tests for `CollectionPaths`

--- firefuse/src/collection.test.ts
+++ firefuse/src/collection.test.ts
@@ -1,10 +1,38 @@
 import * as fs from "firebase/firestore";
-import { Collection } from "./collection.js";
-import { Assert, City, DB, Exact, MySchema } from "./index.test.js";
-
-export const collection = fs.collection as Collection<MySchema>;
+import { CollectionPaths } from "./collection.js";
+import {
+  Assert,
+  City,
+  collection,
+  DB,
+  Exact,
+  Extends,
+  MySchema,
+} from "./index.test.js";
 
 test(`collection(DB, "cities") returuns CollectionReference<City>`, () => {
   const cities = collection(DB, "cities");
   type _ = Assert<Exact<fs.CollectionReference<City>, typeof cities>>;
+});
+
+describe(`CollectionPaths`, () => {
+  test(`["user"] extends CollectionPaths`, () => {
+    type ColPaths = CollectionPaths<MySchema>;
+    type _ = Assert<Extends<["user"], ColPaths>>;
+  });
+
+  test(`["user", string, "favRooms"] extends CollectionPaths`, () => {
+    type ColPaths = CollectionPaths<MySchema>;
+    type _ = Assert<Extends<["user", string, "favRooms"], ColPaths>>;
+  });
+
+  test(`["cities", "v1", "cities"] extends CollectionPaths`, () => {
+    type ColPaths = CollectionPaths<MySchema>;
+    type _ = Assert<Extends<["cities", "v1", "cities"], ColPaths>>;
+  });
+
+  test(`["cities", "v2", "cities"] extends CollectionPaths`, () => {
+    type ColPaths = CollectionPaths<MySchema>;
+    type _ = Assert<Extends<["cities", "v2", "cities"], ColPaths>>;
+  });
 });
