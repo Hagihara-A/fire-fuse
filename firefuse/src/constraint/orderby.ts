a583068136d84e077d0f9e2fcdee97df5e7e73fa
Arark <knkr5456@gmail.com>
2022-03-08 01:34:27 +0900

    feat(orderBy): remove `orderBy` function
    BREAKING CHANGE: `orderBy` is no longer exported

--- firefuse/src/constraint/orderby.ts
+++ firefuse/src/constraint/orderby.ts
@@ -8,19 +8,13 @@ export type KeyofOrderable<T extends DocumentData> = keyof T extends infer K
     ? K
     : never
   : never;
+
 export interface OrderBy<T extends DocumentData> {
   <F extends KeyofOrderable<T> & string>(
     field: F,
     order?: firestore.OrderByDirection
   ): OrderByConstraint<F>;
 }
-
-export const orderBy = <T extends DocumentData>(): OrderBy<T> => {
-  return <F extends KeyofOrderable<T>>(
-    field: F,
-    order?: firestore.OrderByDirection
-  ) => firestore.orderBy(field, order) as OrderByConstraint<F>;
-};
 
 export interface OrderByConstraint<F extends string>
   extends firestore.QueryConstraint {
