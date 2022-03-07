dc85aa20920a71e00f92cc27cbd13843f68a0f37
Arark <knkr5456@gmail.com>
2022-03-08 02:58:27 +0900

    feat(constraint): impl `QueryConstraint` type

---
+++ firefuse/src/constraint/QueryConstraint.ts
@@ -0,0 +1,11 @@
+import { DocumentData, StrKeyof } from "../index.js";
+import { OrderByConstraint } from "./orderby.js";
+import { OtherConstraints } from "./other";
+import { LegalOperation, LegalValue, WhereConstraint } from "./where.js";
+
+export type QueryConstraint<
+  D extends DocumentData,
+  F extends StrKeyof<D> = StrKeyof<D>,
+  OP extends LegalOperation<D, F> = LegalOperation<D, F>,
+  V extends Readonly<LegalValue<D, F, OP>> = Readonly<LegalValue<D, F, OP>>
+> = WhereConstraint<D, F, OP, V> | OrderByConstraint<F> | OtherConstraints;
