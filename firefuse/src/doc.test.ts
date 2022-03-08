2c64c5828474bfaa6e139b29e918b6b751f3c9b5
Arark <knkr5456@gmail.com>
2022-03-09 02:10:26 +0900

    test(doc): assert `doc(colRef)` is OK

--- firefuse/src/doc.test.ts
+++ firefuse/src/doc.test.ts
@@ -1,4 +1,13 @@
-import { Assert, Extends, MySchema, NotExtends, Room } from "./index.test.js";
+import {
+  Assert,
+  collection,
+  DB,
+  doc,
+  Extends,
+  MySchema,
+  NotExtends,
+  Room,
+} from "./index.test.js";
 import { DocumentPaths } from "./doc.js";
 
 describe(`DocumentPaths`, () => {
@@ -10,5 +19,17 @@ describe(`DocumentPaths`, () => {
   test(`["user", string, "favRooms"] not extends DocumentPaths`, () => {
     type P = DocumentPaths<MySchema>;
     type _ = Assert<NotExtends<["user", string, "favRooms"], P>>;
+  });
+});
+
+describe(`doc`, () => {
+  test("doc(colRef) is OK", () => {
+    const colRef = collection(DB, "user");
+    expect(() => doc(colRef)).not.toThrow();
+  });
+
+  test("doc(colRef, 'id') is OK", () => {
+    const colRef = collection(DB, "user");
+    expect(() => doc(colRef, "id")).not.toThrow();
   });
 });
