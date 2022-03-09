931404420f37ff85f9fb13ee7e9c60e0eb0c0bd5
Arark <knkr5456@gmail.com>
2022-03-10 01:15:08 +0900

    feat(CollectionPaths): make `CollectionPaths` take new `Schema`

--- firefuse-admin/src/collection.ts
+++ firefuse-admin/src/collection.ts
@@ -1,15 +1,31 @@
 import { CollectionReference } from "@google-cloud/firestore";
-import { DocumentData, FuseQuery, SchemaBase, StrKeyof } from "./index.js";
+import {
+  DocumentData,
+  FuseQuery,
+  Schema,
+  StrKeyof,
+  ExcUndef,
+} from "./index.js";
 import { FuseDocumentReference } from "./reference.js";
-export type CollectionPaths<S extends SchemaBase> = StrKeyof<S> extends infer K
-  ? K extends StrKeyof<S>
-    ? S[K]["subcollection"] extends SchemaBase
-      ? `${K}` | `${K}/${string}/${CollectionPaths<S[K]["subcollection"]>}`
-      : `${K}`
+
+export type CollectionPaths<S extends Schema> = StrKeyof<S> extends infer ColKey
+  ? ColKey extends StrKeyof<S>
+    ? StrKeyof<S[ColKey]> extends infer DocKey
+      ? DocKey extends StrKeyof<S[ColKey]>
+        ? S[ColKey][DocKey]["col"] extends Schema | undefined
+          ?
+              | `${ColKey}`
+              | `${ColKey}/${DocKey}/${CollectionPaths<
+                  ExcUndef<S[ColKey][DocKey]["col"]>
+                >}`
+          : `${ColKey}`
+        : never
+      : never
     : never
   : never;
 
-type Base<T extends DocumentData> = FuseQuery<T> & Omit<CollectionReference<T>, "where">;
+type Base<T extends DocumentData> = FuseQuery<T> &
+  Omit<CollectionReference<T>, "where">;
 export interface FuseCollectionReference<T extends DocumentData>
   extends Base<T> {
   doc(id?: string): FuseDocumentReference<T>;
