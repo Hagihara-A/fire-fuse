0bb80e8aecbf350a8a81cbbf85a52c64197ca757
Arark <knkr5456@gmail.com>
2022-03-10 01:15:24 +0900

    test(admin/collection): tests for `CollectionPaths`

--- firefuse-admin/src/collection.test.ts
+++ firefuse-admin/src/collection.test.ts
@@ -1,15 +1,43 @@
-import { Assert, City, DB, Exact, Extends, MySchema } from "./index.test.js";
+import {
+  Assert,
+  City,
+  DB,
+  Exact,
+  Extends,
+  MySchema,
+  NotExtends,
+} from "./index.test.js";
 import { CollectionPaths, FuseCollectionReference } from "./collection.js";
 
 describe(`CollectionPaths`, () => {
+  type P = CollectionPaths<MySchema>;
   test(`\`cities\` extends CollectionPaths`, () => {
-    type DP = CollectionPaths<MySchema>;
-    type _ = Assert<Extends<DP, `cities`>>;
+    type _ = Assert<Extends<"cities", P>>;
+  });
+  test(`"user" extends CollectionPaths`, () => {
+    type _ = Assert<Extends<"user", P>>;
+  });
+
+  test(`"user/string/favRooms" extends CollectionPaths`, () => {
+    type _ = Assert<Extends<`user/${string}/favRooms`, P>>;
+  });
+
+  test(`"cities/v1/cities" extends CollectionPaths`, () => {
+    type _ = Assert<Extends<`cities/v1/cities`, P>>;
+  });
+
+  test(`"cities/v2/cities" extends CollectionPaths`, () => {
+    type _ = Assert<Extends<"cities/v2/cities", P>>;
+  });
+
+  test(`"hoge/huga/piyo" not extends CollectionPaths`, () => {
+    type _ = Assert<NotExtends<"hoge/huga/piyo", P>>;
   });
 });
+
 describe(`collection`, () => {
-  test(`DB.collection("cities") returns ColRef<City>`, () => {
-    const cities = DB.collection("cities");
+  test(`DB.collection("cities/v1/cities") returns ColRef<City>`, () => {
+    const cities = DB.collection("cities/v1/cities");
     type _ = Assert<Exact<FuseCollectionReference<City>, typeof cities>>;
   });
 });
