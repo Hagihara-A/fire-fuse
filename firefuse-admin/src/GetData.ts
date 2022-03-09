3f336988a3f4bfedd9a3473c73216f7d760acb9b
Arark <knkr5456@gmail.com>
2022-03-10 01:07:51 +0900

    feat(admin/Schema): specify DocumentKey in Schema
    BREAKING CHANGE: Schema now needs DocumentKey to be specified

---
+++ firefuse-admin/src/GetData.ts
@@ -0,0 +1,19 @@
+import { StrKeyof } from "firefuse";
+import { CollectionPaths } from "./collection.js";
+import { DocumentPaths } from "./doc.js";
+import { Schema, ExcUndef } from "./index.js";
+
+export type GetData<
+  S extends Schema,
+  P extends CollectionPaths<S> | DocumentPaths<S>
+> = P extends `${infer ColKey}/${infer DocKey}/${infer Rest}`
+  ? S[ColKey][DocKey]["col"] extends Schema | undefined
+    ? Rest extends
+        | CollectionPaths<ExcUndef<S[ColKey][DocKey]["col"]>>
+        | DocumentPaths<ExcUndef<S[ColKey][DocKey]["col"]>>
+      ? GetData<ExcUndef<S[ColKey][DocKey]["col"]>, Rest>
+      : never
+    : never
+  : P extends `${infer ColKey}/${infer DocKey}`
+  ? S[ColKey][DocKey]["doc"]
+  : S[P][StrKeyof<S[P]>]["doc"];
