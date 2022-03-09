58ac3522814540fc3328ca356fc6f37ad52dce96
Arark <knkr5456@gmail.com>
2022-03-10 01:06:03 +0900

    test(firefuse): reuse `CollectionPaths` in some tests

--- firefuse/src/collection.test.ts
+++ firefuse/src/collection.test.ts
@@ -1,24 +1,25 @@
 import { CollectionPaths } from "./collection.js";
-import { Assert, Extends, MySchema } from "./index.test.js";
+import { Assert, Extends, MySchema, NotExtends } from "./index.test.js";
 
 describe(`CollectionPaths`, () => {
+  type P = CollectionPaths<MySchema>;
   test(`["user"] extends CollectionPaths`, () => {
-    type ColPaths = CollectionPaths<MySchema>;
-    type _ = Assert<Extends<["user"], ColPaths>>;
+    type _ = Assert<Extends<["user"], P>>;
   });
 
   test(`["user", string, "favRooms"] extends CollectionPaths`, () => {
-    type ColPaths = CollectionPaths<MySchema>;
-    type _ = Assert<Extends<["user", string, "favRooms"], ColPaths>>;
+    type _ = Assert<Extends<["user", string, "favRooms"], P>>;
   });
 
   test(`["cities", "v1", "cities"] extends CollectionPaths`, () => {
-    type ColPaths = CollectionPaths<MySchema>;
-    type _ = Assert<Extends<["cities", "v1", "cities"], ColPaths>>;
+    type _ = Assert<Extends<["cities", "v1", "cities"], P>>;
   });
 
   test(`["cities", "v2", "cities"] extends CollectionPaths`, () => {
-    type ColPaths = CollectionPaths<MySchema>;
-    type _ = Assert<Extends<["cities", "v2", "cities"], ColPaths>>;
+    type _ = Assert<Extends<["cities", "v2", "cities"], P>>;
+  });
+
+  test(`["hoge", "huga", "piyo"] not extends CollectionPaths`, () => {
+    type _ = Assert<NotExtends<["hoge", "huga", "piyo"], P>>;
   });
 });
