f84c0f2013115b86c95b7785bacaa3c4086a73e8
arark <knkr5456@gmail.com>
2023-06-09 12:26:03 +0900

    chore(firefuse): update configs

--- firefuse/jest.config.js
+++ firefuse/jest.config.js
@@ -1,13 +1,20 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-export default {
-  testEnvironment: "node",
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const config = {
+  // [...]
   preset: "ts-jest/presets/default-esm", // or other ESM presets
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
+
+export default config;
