03fbfdc1434b58a86564f146667c1b99719ebbd0
arark <knkr5456@gmail.com>
2023-06-11 02:39:21 +0900

    fix(firefuse): fix overload of `doc()` to handle reference relative to collection

--- firefuse/src/doc.ts
+++ firefuse/src/doc.ts
@@ -1,17 +1,22 @@
 import * as fst from "firebase/firestore";
 
 import { GetData } from "./GetData.js";
-import { DocumentData, Schema, StrKeyof } from "./index.js";
+import { Schema, StrKeyof } from "./index.js";
 
 export interface Doc<S extends Schema> {
+  <T extends StringDocKeyData<S>>(
+    collectionRef: fst.CollectionReference<T>
+  ): fst.DocumentReference<T>;
+
+  <T, P extends ChildDocPath<S, T>>(
+    collectionRef: fst.CollectionReference<T>,
+    id: P
+  ): fst.DocumentReference<T>;
+
   <P extends DocumentPaths<S>>(
     firestore: fst.Firestore,
     ...paths: P
   ): fst.DocumentReference<GetData<S, P>>;
-  <T extends DocumentData>(
-    collectionRef: fst.CollectionReference<T>,
-    id?: string
-  ): fst.DocumentReference<T>;
 }
 
 export type DocumentPaths<S extends Schema> = StrKeyof<S> extends infer ColKey
@@ -27,3 +32,36 @@ export type DocumentPaths<S extends Schema> = StrKeyof<S> extends infer ColKey
       : never
     : never
   : never;
+
+export type ChildDocPath<S extends Schema, T> = StrKeyof<S> extends infer ColKey
+  ? ColKey extends keyof S
+    ? StrKeyof<S[ColKey]> extends infer DocKey
+      ? DocKey extends keyof S[ColKey]
+        ? T extends S[ColKey][DocKey]["doc"]
+          ? DocKey
+          : S[ColKey][DocKey]["col"] extends Schema
+          ? ChildDocPath<S[ColKey][DocKey]["col"], T>
+          : never
+        : never
+      : never
+    : never
+  : never;
+
+export type StringDocKeyData<S extends Schema> =
+  StrKeyof<S> extends infer ColKey
+    ? ColKey extends keyof S
+      ? StrKeyof<S[ColKey]> extends infer DocKey
+        ? DocKey extends keyof S[ColKey]
+          ? S[ColKey][DocKey]["col"] extends Schema
+            ? string extends DocKey
+              ?
+                  | S[ColKey][DocKey]["doc"]
+                  | StringDocKeyData<S[ColKey][DocKey]["col"]>
+              : StringDocKeyData<S[ColKey][DocKey]["col"]>
+            : string extends DocKey
+            ? S[ColKey][DocKey]["doc"]
+            : never
+          : never
+        : never
+      : never
+    : never;
