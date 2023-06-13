ca22963e8acd8f22a6f76febabdcbc51e93ff199
arark <knkr5456@gmail.com>
2023-06-13 21:50:29 +0900

    refactor(firefuse/utils): remove `DocumentData` and `StrKeyof` from type constraint

--- firefuse/src/utils.ts
+++ firefuse/src/utils.ts
@@ -1,8 +1,6 @@
-import { DocumentData } from "./index.js";
-
 export type ExcUndef<T> = Exclude<T, undefined>;
 
-export type Defined<T extends DocumentData, K extends StrKeyof<T>> = T & {
+export type Defined<T, K extends keyof T> = T & {
   [L in K]-?: ExcUndef<T[K]>;
 };
 
