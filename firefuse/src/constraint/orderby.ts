170d20391be596c82761902e87826da21706a4a5
arark <knkr5456@gmail.com>
2023-06-14 00:05:04 +0900

    refactor(firefuse): remove unecessary type constrainst of `OrderBy`

--- firefuse/src/constraint/orderby.ts
+++ firefuse/src/constraint/orderby.ts
@@ -1,22 +1,13 @@
 import * as fst from "firebase/firestore";
 
-import { DocumentData } from "../index.js";
-import { Orderable } from "./where.js";
-
-export type KeyofOrderable<T extends DocumentData> = keyof T extends infer K
-  ? K extends Orderable
-    ? K
-    : never
-  : never;
-
-export interface OrderBy<T extends DocumentData> {
-  <F extends KeyofOrderable<T> & string>(
+export interface OrderBy<T> {
+  <F extends keyof T>(
     field: F,
     order?: fst.OrderByDirection
-  ): OrderByConstraint<F>;
+  ): OrderByConstraint<T, F>;
 }
 
-export interface OrderByConstraint<F extends string>
+export interface OrderByConstraint<T, F extends keyof T>
   extends fst.QueryConstraint {
   readonly type: Extract<fst.QueryConstraintType, "orderBy">;
   field: F;
