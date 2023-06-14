e4792dfb07e1ecf3c91c5284c84ba077fc2267ca
arark <knkr5456@gmail.com>
2023-06-14 21:11:40 +0900

    refactor(firefuse-admin): remove unecessary type constraint in `GetData`

--- firefuse-admin/src/GetData.ts
+++ firefuse-admin/src/GetData.ts
@@ -1,16 +1,12 @@
-import { CollectionPaths } from "./collection.js";
-import { DocumentPaths } from "./doc.js";
 import { Schema } from "./index.js";
 import { ExcUndef, StrKeyof } from "./utils.js";
 
 export type GetData<
   S extends Schema,
-  P extends CollectionPaths<S> | DocumentPaths<S>
+  P extends string
 > = P extends `${infer ColKey}/${infer DocKey}/${infer Rest}`
   ? S[ColKey][DocKey]["col"] extends Schema | undefined
-    ? Rest extends
-        | CollectionPaths<ExcUndef<S[ColKey][DocKey]["col"]>>
-        | DocumentPaths<ExcUndef<S[ColKey][DocKey]["col"]>>
+    ? Rest extends string
       ? GetData<ExcUndef<S[ColKey][DocKey]["col"]>, Rest>
       : never
     : never
