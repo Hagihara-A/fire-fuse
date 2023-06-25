08dbef3ccfd6e1f2a22f960c1b9afbfb449d26d9
arark <knkr5456@gmail.com>
2023-06-25 21:41:27 +0900

    fix(firefuse-admin): fix treating `undefined` is assignable to Schema

--- firefuse-admin/src/doc.ts
+++ firefuse-admin/src/doc.ts
@@ -5,13 +5,11 @@ export type DocumentPaths<S extends Schema> = StrKeyof<S> extends infer ColKey
   ? ColKey extends StrKeyof<S>
     ? StrKeyof<S[ColKey]> extends infer DocKey
       ? DocKey extends StrKeyof<S[ColKey]>
-        ? S[ColKey][DocKey]["col"] extends Schema | undefined
-          ?
-              | `${ColKey}/${DocKey}`
-              | `${ColKey}/${DocKey}/${DocumentPaths<
-                  ExcUndef<S[ColKey][DocKey]["col"]>
-                >}`
-          : `${ColKey}/${DocKey}`
+        ? ExcUndef<S[ColKey][DocKey]["col"]> extends infer SS
+          ? SS extends Schema
+            ? `${ColKey}/${DocKey}` | `${ColKey}/${DocKey}/${DocumentPaths<SS>}`
+            : `${ColKey}/${DocKey}`
+          : never
         : never
       : never
     : never
